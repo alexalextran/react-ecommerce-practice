761cd02cb609c02b441d44fc0a463bad79cce632
alexalextran <alextran8385@gmail.com>
2022-01-14 17:53:31 +1100

    first commit

---
+++ src/components/Landing.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import UndrawBooks from '../assets/Undraw_Books/Nav'
+
+const Landing = () => {
+    return (
+        <div id='landing'>
+        <header>
+            <div className='header__container'>
+                <div className='header__description'>
+                    <h1>
+                        Australia's most awarded online library platform
+                    </h1>
+
+                    <h2>
+                        Find your dream book with <span className='purple'>Library</span>
+                    </h2>
+
+                    <a href='#features'>
+                    <button className='btn'>Browse Books</button>
+                       
+                    </a>
+                </div>
+                <figure className='header__img--wrapper'>
+                    <img src={UndrawBooks}/>
+                </figure>
+            </div>
+        </header>
+            
+        </div>
+    );
+}
+
+export default Landing;
